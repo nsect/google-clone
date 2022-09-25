b01bcfa653cd84fa4b39782f85d3d899f385d9f4
nsect <nsecttheory@gmail.com>
2022-09-24 18:23:22 -0700

    remove dev dummy data

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import SearchResults from '../components/SearchResults';
 import { API_KEY, CONTEXT_KEY } from '../config';
-import Response from '../Response';
 
 function Search({ results }) {
     const router = useRouter();
@@ -28,9 +27,7 @@ export async function getServerSideProps(context) {
   const useDummyData = false;
   const startIndex = context.query.start || '0';
 
-  const data = useDummyData
-    ? Response
-    : await fetch(
+  const data = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
       ).then((response) => response.json());
 
